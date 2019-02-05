ae269d4f74b754b1a91b41b5d0d58ded3d581f64
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-05 16:00:40 -0500

    Add createChatMessages()

--- owl/src/index.js
+++ owl/src/index.js
@@ -5,47 +5,125 @@ import App from './App';
 import Stage from './Stage';
 import * as serviceWorker from './serviceWorker';
 
-const tempData = [
-  {
-      id: 1,
-      name: 'Koren Templeton',
-      avatar:
-          'https://robohash.org/itaquevoluptatumrerum.jpg?size=200x200&set=set1',
-      inSession: true,
-      onStage: true
-  },
-  {
-      id: 2,
-      name: 'Caty Flucker',
-      avatar:
-          'https://robohash.org/doloredolorescupiditate.jpg?size=200x200&set=set1',
-      inSession: true,
-      onStage: false
-  },
-  {
-      id: 3,
-      name: 'Axe Kubicka',
-      avatar: 'https://robohash.org/utlaborumfugit.jpg?size=200x200&set=set1',
-      inSession: false,
-      onStage: false
-  },
-  {
-      id: 4,
-      name: 'Frank Runciman',
-      avatar:
-          'https://robohash.org/etexercitationemassumenda.jpg?size=200x200&set=set1',
-      inSession: true,
-      onStage: false
-  },
-  {
-      id: 5,
-      name: 'Ashla Attwool',
-      avatar:
-          'https://robohash.org/iustodoloremqueinventore.jpg?size=200x200&set=set1',
-      inSession: true,
-      onStage: true
-  }
+const participants = [
+    {
+        id: 1,
+        name: 'Koren Templeton',
+        avatar:
+            'https://robohash.org/itaquevoluptatumrerum.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: true
+    },
+    {
+        id: 2,
+        name: 'Caty Flucker',
+        avatar:
+            'https://robohash.org/doloredolorescupiditate.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: false
+    },
+    {
+        id: 3,
+        name: 'Axe Kubicka',
+        avatar: 'https://robohash.org/utlaborumfugit.jpg?size=200x200&set=set1',
+        inSession: false,
+        onStage: false
+    },
+    {
+        id: 4,
+        name: 'Frank Runciman',
+        avatar:
+            'https://robohash.org/etexercitationemassumenda.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: false
+    },
+    {
+        id: 5,
+        name: 'Ashla Attwool',
+        avatar:
+            'https://robohash.org/iustodoloremqueinventore.jpg?size=200x200&set=set1',
+        inSession: true,
+        onStage: true
+    }
 ];
+const chatEvents = [
+    {
+        participantId: 3,
+        type: 'message',
+        message: 'Hello world',
+        time: 1548852646559,
+        timestamp: 1548852484247
+    },
+    {
+        participantId: 2,
+        type: 'thumbs-up',
+        timestamp: 1548852484247
+    },
+    {
+        participantId: 4,
+        type: 'thumbs-down',
+        timestamp: 1548852484247
+    },
+    {
+        participantId: 1,
+        type: 'raise-hand',
+        timestamp: 1548852544247
+    },
+    {
+        participantId: 1,
+        type: 'clap',
+        timestamp: 1548852544247
+    },
+    {
+        participantId: 1,
+        type: 'join',
+        timestamp: 1548852544247
+    },
+    {
+        participantId: 5,
+        type: 'leave',
+        timestamp: 1548852604247
+    },
+    {
+        participantId: 3,
+        type: 'join-stage',
+        timestamp: 1548852664247
+    },
+    {
+        participantId: 3,
+        type: 'leave-stage',
+        timestamp: 1548852724247
+    }
+];
+
+const createChatMessages = function (participants, events) {
+
+    return chatEvents.map((e) => {
+
+    const expanded = {};
+
+    // Convert participantID into participant object
+    expanded.participant = participants.find(p => {
+      return p.id = e.participantId;
+    });
+
+    // Add required attributes
+    expanded.type = e.type;
+    expanded.timestamp = e.timestamp;
+
+    // Add optional attributes
+    if (e.message) { expanded.message = e.message; }
+    if (e.time) { expanded.time = e.time; }
+
+    return expanded;
+  });
+};
+
+const tempData = createChatMessages(participants, chatEvents);
+
+console.log(tempData);
+
+
 const test = <Stage users={tempData} />
 
 ReactDOM.render(test, document.getElementById('root'));
